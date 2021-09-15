b2d53b4d538e15de5d73eb3da15935a7f3808634
SaidCosovic <saidcosovic5@gmail.com>
2021-09-15 20:06:07 +0200

    ex05

---
+++ ex05/src/index.js
@@ -0,0 +1,14 @@
+var fooBoo = 0;
+
+function main() {
+    // Only change code below this line
+
+    // Only change code above this line
+    return x;
+
+    }
+
+    console.log(main());
+
+    module.exports = main;
+    
