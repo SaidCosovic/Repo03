6848fe2c309e63ad9790e3039eabce44d2d00450
SaidCosovic <saidcosovic5@gmail.com>
2021-09-15 20:17:01 +0200

    ex06

---
+++ ex06/src/index.js
@@ -0,0 +1,29 @@
+//Only change code below this line
+var x;
+
+var y;
+
+var z;
+// Only change code above this line
+
+
+x = x + 2;
+
+y = y + 4;
+z = z + " John!";
+
+function main() {
+// Only change code below this line
+
+// Only change code above this line
+
+x = x + 2;
+
+y = y + 4;
+z = z + " John!";
+return [x, y, z];
+
+}
+
+console.log(main());
+module.exports = main;
